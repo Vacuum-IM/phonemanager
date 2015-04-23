79fb1de632ed958de29e5dbf4aea3a0cd16dc03b
Sergey Potapov <potapov.s.a@gmail.com>
2015-04-23 20:45:37 +0300

    Новый подход в работе с локализациями.

---
+++ translations/es/phonemanager.ts
@@ -0,0 +1,307 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" sourcelanguage="dua" version="2.0">
+<context>
+    <name>CallHistoryViewWidget</name>
+    <message>
+        <source>Yesterday</source>
+        <translation>Ayer</translation>
+    </message>
+</context>
+<context>
+    <name>CallHistoryWindow</name>
+    <message>
+        <source>Calls History</source>
+        <translation>Historial de llamadas</translation>
+    </message>
+    <message>
+        <source>Clear History</source>
+        <translation>Limpiar historial</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <source>Today</source>
+        <translation>Hoy</translation>
+    </message>
+    <message>
+        <source>Yesterday</source>
+        <translation>Ayer</translation>
+    </message>
+    <message>
+        <source>Last 7 Days</source>
+        <translation>Los últimos 7 días</translation>
+    </message>
+    <message>
+        <source>Last Month</source>
+        <translation>El último mes</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete all of your call history? This operation can not be undone.</source>
+        <translation>¿Estás seguro de que quieres eliminar todo tu historial de llamadas? Esta operación no se puede deshacer.</translation>
+    </message>
+</context>
+<context>
+    <name>DialerWidget</name>
+    <message>
+        <source>Enter number</source>
+        <translation>Escribe el número</translation>
+    </message>
+</context>
+<context>
+    <name>EditNumbersWidget</name>
+    <message>
+        <source>Number:</source>
+        <translation>Número:</translation>
+    </message>
+    <message>
+        <source>Append</source>
+        <translation>Añadir</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Borrar</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Remove Phone Number</source>
+        <translation>Borrar el número telefónico</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete the number &lt;b&gt;%1&lt;/b&gt;?</source>
+        <translation>¿Estás seguro de que quieres borrar el número &lt;b&gt;%1&lt;/b&gt;?</translation>
+    </message>
+</context>
+<context>
+    <name>IncomingCallDialog</name>
+    <message>
+        <source>Incoming Call</source>
+        <translation>Llamada entrante</translation>
+    </message>
+    <message>
+        <source>Answer</source>
+        <translation>Responder</translation>
+    </message>
+    <message>
+        <source>Answer with Video</source>
+        <translation>Responder con vídeo</translation>
+    </message>
+</context>
+<context>
+    <name>PhoneCall</name>
+    <message>
+        <source>Callee is unavailable</source>
+        <translation>El subscriptor no está disponible</translation>
+    </message>
+    <message>
+        <source>Invalid SIP Address &apos;%1&apos;</source>
+        <translation>Dirección SIP inválida &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>Failed to register at SIP server</source>
+        <translation>Error al registrarse con el servidor SIP</translation>
+    </message>
+</context>
+<context>
+    <name>PhoneCallWidget</name>
+    <message>
+        <source>Calling...</source>
+        <translation>Llamando...</translation>
+    </message>
+    <message>
+        <source>Connecting...</source>
+        <translation>Conectando...</translation>
+    </message>
+    <message>
+        <source>Caller is busy.</source>
+        <translation>Extensión ocupada.</translation>
+    </message>
+    <message>
+        <source>Call finished.</source>
+        <translation>Llamada terminada.</translation>
+    </message>
+    <message>
+        <source>Call declined.</source>
+        <translation>Llamada rechazada.</translation>
+    </message>
+    <message>
+        <source>Call not accepted.</source>
+        <translation>Llamada no aceptada.</translation>
+    </message>
+    <message>
+        <source>Call accepted.</source>
+        <translation>Llamada aceptada.</translation>
+    </message>
+    <message>
+        <source>Call failed. Try to call later.</source>
+        <translation>Llamada fallida. Intenta llamar más tarde.</translation>
+    </message>
+</context>
+<context>
+    <name>PhoneCallWindow</name>
+    <message>
+        <source>Call with %1</source>
+        <translation>Llamada con %1</translation>
+    </message>
+</context>
+<context>
+    <name>PhoneChatHandler</name>
+    <message>
+        <source>Call</source>
+        <translation>Llamada</translation>
+    </message>
+    <message>
+        <source>Video Call</source>
+        <translation>Llamada de vídeo</translation>
+    </message>
+    <message>
+        <source>Edit Numbers...</source>
+        <translation>Editar números...</translation>
+    </message>
+</context>
+<context>
+    <name>PhoneDialerDialog</name>
+    <message>
+        <source>Calls</source>
+        <translation>Llamadas</translation>
+    </message>
+    <message>
+        <source>Call</source>
+        <translation>Llamada</translation>
+    </message>
+    <message>
+        <source>Full history of calls</source>
+        <translation>Historial de llamadas completo</translation>
+    </message>
+</context>
+<context>
+    <name>PhoneManager</name>
+    <message>
+        <source>Phone Manager</source>
+        <translation>Administrador de teléfono</translation>
+    </message>
+    <message>
+        <source>Allows to call and send SMS messages to phones</source>
+        <translation>Permite realizar llamadas y enviar mensajes SMS a teléfonos</translation>
+    </message>
+    <message>
+        <source>Failed to create database</source>
+        <translation>Error al crear la base de datos</translation>
+    </message>
+    <message>
+        <source>Failed to open database</source>
+        <translation>Error al abrir la base de datos</translation>
+    </message>
+    <message>
+        <source>Database format is not compatible</source>
+        <translation>El formato de la base de datos no es compatible</translation>
+    </message>
+    <message>
+        <source>Failed to to execute SQL query</source>
+        <translation>Error al ejecutar la consulta SQL</translation>
+    </message>
+    <message>
+        <source>When incoming call not answered</source>
+        <translation>Al no responder una llamada entrante</translation>
+    </message>
+    <message>
+        <source>%1 is now talking. Call later.</source>
+        <translation>%1 ahora está ocupado. Inténtalo más tarde.</translation>
+    </message>
+    <message>
+        <source>Call finished (duration: %1).</source>
+        <translation>Llamada terminada (duración: %1)</translation>
+    </message>
+    <message>
+        <source>Missed call.</source>
+        <translation>Llamada perdida.</translation>
+    </message>
+    <message>
+        <source>Call canceled.</source>
+        <translation>Llamada cancelada.</translation>
+    </message>
+    <message>
+        <source>Call not accepted.</source>
+        <translation>Llamada no aceptada.</translation>
+    </message>
+    <message>
+        <source>Call accepted.</source>
+        <translation>Llamada aceptada.</translation>
+    </message>
+    <message>
+        <source>Call failed. Try to call later.</source>
+        <translation>Llamada fallida. Intenta más tarde.</translation>
+    </message>
+    <message>
+        <source>Missed call from %1</source>
+        <translation>Llamada perdida de %1</translation>
+    </message>
+    <message>
+        <source>Missed Call</source>
+        <translation>Llamada perdida</translation>
+    </message>
+    <message>
+        <source>Call Back</source>
+        <translation>Volver a llamar</translation>
+    </message>
+    <message>
+        <source>Chat</source>
+        <translation>Escribir</translation>
+    </message>
+    <message>
+        <source>SIP Phone</source>
+        <translation>Teléfono SIP</translation>
+    </message>
+    <message>
+        <source>Voice and video calls over SIP</source>
+        <translation>Llamadas de voz y vídeo sobre SIP</translation>
+    </message>
+    <message>
+        <source>Account</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Login:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Connection</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>SIP Server:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Port:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>SIP Proxy:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Media streams</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>STUN Server:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Enable ICE negotiation</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
---
+++ translations/pl/phonemanager.ts
@@ -0,0 +1,307 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="pl" sourcelanguage="dua" version="2.0">
+<context>
+    <name>CallHistoryViewWidget</name>
+    <message>
+        <source>Yesterday</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>CallHistoryWindow</name>
+    <message>
+        <source>Calls History</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Clear History</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Today</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Yesterday</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Last 7 Days</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Last Month</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Are you sure you want to delete all of your call history? This operation can not be undone.</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>DialerWidget</name>
+    <message>
+        <source>Enter number</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>EditNumbersWidget</name>
+    <message>
+        <source>Number:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Append</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Remove Phone Number</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Are you sure you want to delete the number &lt;b&gt;%1&lt;/b&gt;?</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>IncomingCallDialog</name>
+    <message>
+        <source>Incoming Call</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Answer</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Answer with Video</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>PhoneCall</name>
+    <message>
+        <source>Callee is unavailable</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Invalid SIP Address &apos;%1&apos;</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Failed to register at SIP server</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>PhoneCallWidget</name>
+    <message>
+        <source>Calling...</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Connecting...</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Caller is busy.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Call finished.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Call declined.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Call not accepted.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Call accepted.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Call failed. Try to call later.</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>PhoneCallWindow</name>
+    <message>
+        <source>Call with %1</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>PhoneChatHandler</name>
+    <message>
+        <source>Call</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Video Call</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Edit Numbers...</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>PhoneDialerDialog</name>
+    <message>
+        <source>Calls</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Call</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Full history of calls</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>PhoneManager</name>
+    <message>
+        <source>Phone Manager</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Allows to call and send SMS messages to phones</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Failed to create database</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Failed to open database</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Database format is not compatible</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Failed to to execute SQL query</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>When incoming call not answered</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>%1 is now talking. Call later.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Call finished (duration: %1).</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Missed call.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Call canceled.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Call not accepted.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Call accepted.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Call failed. Try to call later.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Missed call from %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Missed Call</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Call Back</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Chat</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>SIP Phone</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Voice and video calls over SIP</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Account</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Login:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Connection</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>SIP Server:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Port:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>SIP Proxy:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Media streams</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>STUN Server:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Enable ICE negotiation</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
